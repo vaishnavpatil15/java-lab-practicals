116dca2cef7c7573ccb79864806b64dfd0a3a34d
vaishnav <150697688+vaishnavpatil15@users.noreply.github.com>
2024-07-24 22:28:41 -0700

    Add files via upload

--- Matrix.java
+++ Matrix.java
@@ -36,23 +36,85 @@ class matrixx
 		{
 			for(int j=0;j<2;j++)
 			{
-				System.out.print(a[i][j]+" ");
+				System.out.print(" " + a[i][j]+" ");
 			}
 			System.out.println();
 		}
-		System.out.println("\n Matrix 1 :");
+		System.out.println("\n Matrix 2 :");
 
 		for (int i=0;i<2;i++)
 		{
 			for(int j=0;j<2;j++)
 			{
-				System.out.print(b[i][j]+" ");
+				System.out.print(" " + b[i][j]+" ");
 			}
 			System.out.println();
 		}
 
 
 	}
+
+	void add()
+	{
+		System.out.println("\n\n Addition :");
+		for (int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				int c=a[i][j]+b[i][j];
+				System.out.print(" " + c + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	void sub()
+	{
+		System.out.println("\n\n Difference :");
+		for (int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				int c=a[i][j]-b[i][j];
+				System.out.print(" " + c + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	void mul()
+	{
+		System.out.println("\n\n Multiplication :");
+		for (int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				int c=a[i][j]*b[i][j];
+				System.out.print(" " + c + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	void div()
+	{
+		System.out.println("\n\n Division :");
+		for (int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				int c=a[i][j]/b[i][j];
+				System.out.print(" " + c + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+
 
 	
 
@@ -65,6 +127,12 @@ public static void main (String []   args)
 	matrixx m= new matrixx();
 	m.get();
 	m.print();
+
+	m.add();
+	m.sub();
+	m.mul();
+	m.div();
+
 
 }
 }
